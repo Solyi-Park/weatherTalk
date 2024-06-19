78b6b7f6c7080d59855e4db63053a81e41451baa
Solyi Park <solyipark@Solyiui-MacBookPro.local>
2024-06-19 14:44:04 +0900

    useAvatar 로직 분리

--- src/app/components/CasterAvatar.tsx
+++ src/app/components/CasterAvatar.tsx
@@ -1,42 +1,15 @@
 "use client";
 import Image from "next/image";
-import { useCaster } from "../context/CasterContext";
-import { Caster } from "../service/openai";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import ModalPortal from "./ModalPortal";
-import CasterOptions, { CASTER_KEY } from "./CasterOptions";
+import CasterOptions from "./CasterOptions";
 import CasterModal from "./CasterModal";
-import { FULLCASTERS, CasterDetail } from "../data/casters";
+import useAvatar from "../hooks/caster";
 
 export default function CasterAvatar() {
   const [openModal, setOpenModal] = useState(false);
-  const { caster, setCaster } = useCaster();
-  const [currentCaster, setCurrentCaster] = useState<CasterDetail | null>(null);
+  const { caster, currentCaster, selectRandomCaster } = useAvatar();
 
-  const randomIndex = Math.floor(Math.random() * FULLCASTERS.length);
-
-  useEffect(() => {
-    const savedCaster = localStorage.getItem(CASTER_KEY);
-    if (savedCaster) {
-      const foundCaster = FULLCASTERS.find((c) => c.name === savedCaster);
-      if (foundCaster) {
-        setCurrentCaster(foundCaster);
-        setCaster(foundCaster.name);
-      }
-    } else {
-      const randomCaster = FULLCASTERS[randomIndex];
-      setCurrentCaster(randomCaster);
-      setCaster(randomCaster.name);
-      localStorage.setItem(CASTER_KEY, randomCaster.name);
-    }
-  }, [setCaster]);
-
-  const onClickAvatar = () => {
-    const newCaster = FULLCASTERS[randomIndex];
-    setCurrentCaster(newCaster);
-    setCaster(newCaster.name);
-    localStorage.setItem(CASTER_KEY, newCaster.name);
-  };
   return (
     <section className="flex flex-col py-2">
       {currentCaster && (
@@ -46,7 +19,7 @@ export default function CasterAvatar() {
             src={currentCaster.src}
             alt={`image of ${currentCaster.name} `}
             fill
-            onClick={onClickAvatar}
+            onClick={() => selectRandomCaster}
           />
         </div>
       )}
---
+++ src/app/hooks/caster.ts
@@ -0,0 +1,33 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useCaster } from "../context/CasterContext";
+import { CasterDetail, FULLCASTERS } from "../data/casters";
+import { CASTER_KEY } from "../components/CasterOptions";
+
+export default function useAvatar() {
+  const { caster, setCaster } = useCaster();
+  const [currentCaster, setCurrentCaster] = useState<CasterDetail | null>(null);
+
+  const selectRandomCaster = () => {
+    const randomIndex = Math.floor(Math.random() * FULLCASTERS.length);
+    const randomCaster = FULLCASTERS[randomIndex];
+    setCurrentCaster(randomCaster);
+    setCaster(randomCaster.name);
+    localStorage.setItem(CASTER_KEY, randomCaster.name);
+  };
+
+  useEffect(() => {
+    const savedCaster = localStorage.getItem(CASTER_KEY);
+    if (savedCaster) {
+      const foundCaster = FULLCASTERS.find((c) => c.name === savedCaster);
+      if (foundCaster) {
+        setCurrentCaster(foundCaster);
+        setCaster(foundCaster.name);
+      }
+    } else {
+      selectRandomCaster();
+    }
+  }, [setCaster]);
+
+  return { caster, currentCaster, selectRandomCaster };
+}
