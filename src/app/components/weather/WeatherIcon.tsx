a104cb454d7b79140a3f4477361552fc8fbee2db
Solyi Park <solyipark@Solyiui-MacBookPro.local>
2024-06-27 01:32:36 +0900

    icon image 문제 해결중

--- src/app/components/weather/WeatherIcon.tsx
+++ src/app/components/weather/WeatherIcon.tsx
@@ -9,7 +9,7 @@ type Props = {
 export default function WeatherIcon({ icon, size = "small" }: Props) {
   return (
     <Image
-      src={`/api/weather-icon?icon=${icon}`}
+      src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
       width={getIconSize(size)}
       height={getIconSize(size)}
       alt="weather icon"
